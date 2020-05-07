69a9596ef20ca4be8130da56716424c0d6499f2c
rosty <50597942+noname321rosty@users.noreply.github.com>
2020-05-07 23:54:50 +0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,13 @@
+function color(theme){
+    console.log('your theme is = ' + theme )
+    document.getElementById('bg').style.background = theme;
+    document.getElementById('bg2').style.background = theme;
+    document.getElementById('bg3').style.background = theme;
+    document.getElementById('bg4').style.background = theme;
+    document.getElementById('bg5').style.background = theme;
+   
+}
+
+
+  
+
